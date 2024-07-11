2e19f44d30c7efb43efafb1ba34422c414f32a85
ezd_tonny <86495681+antoniogzov@users.noreply.github.com>
2024-07-11 14:02:55 -0600

    UPDATES

---
+++ php/views/modules/automatic_load_pendings/automatic_load_pendings.php
@@ -0,0 +1,69 @@
+<?php
+$assignments = $structure_model->getAssignmentsForPendings();
+$ass_count = 0;
+?>
+<main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section">
+        <div class="hero-bg">
+            <img src="assets/img/hero-bg-light.webp" alt="">
+        </div>
+        <div class="container text-center">
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <h1 data-aos="fade-up">Gestión de <span>Estructura de evaluación</span></h1>
+            </div>
+        </div>
+
+    </section><!-- /Hero Section -->
+
+    <!-- Featured Services Section -->
+    <section id="featured-services" class="featured-services section light-background">
+
+        <div class="container">
+            <h1>Asignaturas sin Estructura de evaluación</h1>
+            <br>
+            <div class="table-responsive">
+                <button class="btn btn-lg btn-primary"><i class="fas fa-sync"></i> Actualizar todas las estructuras pendientes </button>
+                <br>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">ID</th>
+                            <th scope="col">Materia</th>
+                            <th scope="col">Grupo</th>
+                            <th scope="col">Nivel Académico</th>
+                            <th scope="col">Profesor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if (!empty($assignments)) : ?>
+                            <?php foreach ($assignments as $assignment) :
+                                $ass_count++;
+                            ?>
+                                <tr>
+                                    <th scope="row"><?= $ass_count ?></th>
+                                    <th scope="row"><?= $assignment->id_assignment ?></th>
+                                    <td><?= $assignment->name_subject ?></td>
+                                    <td><?= $assignment->group_code ?></td>
+                                    <td><?= $assignment->degree ?></td>
+                                    <td><?= $assignment->teacher_name ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <tr>
+                                <td colspan="8" style="text-align: center; vertical-align: middle;">
+                                    <h1>SIN RESULTADOS</h1>
+                                </td>
+                            </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </section><!-- /Featured Services Section -->
+
+</main>
+<script src="js/updateStructure.js"></script>
